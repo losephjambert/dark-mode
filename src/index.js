6a13789f9113035bb2eab041c48c6bd363eb3da7
Joe <josephc.lambert@gmail.com>
2019-10-02 14:47:49 -0700

    adds routes directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/index.routes';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  rootElement
+);
